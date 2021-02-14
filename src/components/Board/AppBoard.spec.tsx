d7158430c79a209cdde8c6f4321861580d7821ea
Patrick Ng <ngdustry@gmail.com>
2021-02-14 11:36:47 -0500

    testing: fix Dnd test errors

--- src/components/Board/AppBoard.spec.tsx
+++ src/components/Board/AppBoard.spec.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
+import { DndProvider, HTML5Backend } from 'shared/libs';
 import { AppBoard } from './AppBoard';
 
 describe('AppBoard', () => {
   it('should render', () => {
-    const { getByTestId } = render(<AppBoard />);
+    const { getByTestId } = render(
+      <DndProvider backend={HTML5Backend}>
+        <AppBoard />
+      </DndProvider>
+    );
     expect(getByTestId('app-board')).toBeInTheDocument();
   });
 });
--- src/components/Board/List/BoardList.spec.tsx
+++ src/components/Board/List/BoardList.spec.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+
 import { tasks } from 'shared/testing';
+import { DndProvider, HTML5Backend } from 'shared/libs';
 
 import { BoardList } from './BoardList';
 
 describe('BoardList', () => {
   it('should render', () => {
     const { getByTestId } = render(
-      <BoardList tasks={tasks()} title="To Do" status="active" />
+      <DndProvider backend={HTML5Backend}>
+        <BoardList tasks={tasks()} title="To Do" status="active" />
+      </DndProvider>
     );
     expect(getByTestId('board-list')).toBeInTheDocument();
   });
