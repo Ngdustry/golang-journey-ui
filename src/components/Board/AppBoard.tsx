5942514b872b2397f2a0ee59cf2c2e7280559599
Patrick Ng <ngdustry@gmail.com>
2021-03-08 02:15:34 -0500

    feat: renders nothing if loading

--- src/components/Board/AppBoard.tsx
+++ src/components/Board/AppBoard.tsx
@@ -1,7 +1,10 @@
 import React, { FC, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import { Container, Row, Col } from 'shared/libs';
 import { fetchTasks } from 'shared/utils/Http';
 import { Task } from 'shared/types';
+import { validateToken } from 'shared/utils/Auth';
 
 import { BoardList } from 'components/Board/List/BoardList';
 
@@ -12,32 +15,46 @@ const titles = [
 ];
 
 export const AppBoard: FC<unknown> = () => {
+  const [loading, setLoading] = useState<Boolean>(true);
   const [tasks, setTasks] = useState<Task[]>([]);
+  const history = useHistory();
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchTasks();
       setTasks(data);
+      setLoading(false);
     };
 
-    fetchData();
+    const checkToken = async () => {
+      const status = await validateToken();
+
+      if (!status) history.push('/login');
+      else fetchData();
+    };
+
+    checkToken();
   }, []);
 
   return (
-    <div data-testid="app-board" className="app-board">
-      <Container fluid>
-        <Row>
-          {titles.map(({ title, status }) => (
-            <Col key={status} md="4">
-              <BoardList
-                title={title}
-                status={status}
-                tasks={tasks.filter(task => task.status === status)}
-              />
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    </div>
+    <>
+      {loading ? null : (
+        <div data-testid="app-board" className="app-board">
+          <Container fluid>
+            <Row>
+              {titles.map(({ title, status }) => (
+                <Col key={status} md="4">
+                  <BoardList
+                    title={title}
+                    status={status}
+                    tasks={tasks.filter(task => task.status === status)}
+                  />
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        </div>
+      )}
+    </>
   );
 };
--- src/components/Nav/AppNavbar.tsx
+++ src/components/Nav/AppNavbar.tsx
@@ -1,9 +1,13 @@
 import React, { FC, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import { Image, Navbar, Nav } from 'shared/libs';
 import { fetchHandler } from 'shared/utils/Http';
 
 export const AppNavbar: FC<unknown> = () => {
+  const [loading, setLoading] = useState<Boolean>(true);
   const [img, setImg] = useState<string>('');
+  const history = useHistory();
 
   useEffect(() => {
     const fetchImg = async () => {
@@ -11,31 +15,43 @@ export const AppNavbar: FC<unknown> = () => {
       const { thumbnail } = results[0].picture;
 
       setImg(thumbnail);
+      setLoading(false);
     };
 
     fetchImg();
   }, []);
 
+  const handleLogout = () => {
+    localStorage.removeItem('google');
+
+    history.push('/login');
+    window.location.reload();
+  };
+
   return (
-    <Navbar
-      data-testid="app-navbar"
-      className="app-navbar"
-      sticky="top"
-      collapseOnSelect
-      expand="lg"
-    >
-      <Navbar.Brand className="text-light" href="#home">
-        Journey
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Link className="text-light" href="#deets">
-            Log Out
-          </Nav.Link>
-          <Image src={img} roundedCircle />
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      {loading ? null : (
+        <Navbar
+          data-testid="app-navbar"
+          className="app-navbar"
+          sticky="top"
+          collapseOnSelect
+          expand="lg"
+        >
+          <Navbar.Brand className="text-light" href="#home">
+            Journey
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link className="text-light" onClick={handleLogout}>
+                Log Out
+              </Nav.Link>
+              <Image src={img} roundedCircle />
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      )}
+    </>
   );
 };
