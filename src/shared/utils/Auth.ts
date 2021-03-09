a1e111661243ffcb4222fcf320837276220fb638
Patrick Ng <ngdustry@gmail.com>
2021-03-09 01:38:03 -0500

    feat: added util to clear token

--- src/shared/utils/Auth.ts
+++ src/shared/utils/Auth.ts
@@ -1,14 +1,22 @@
+const clearToken = () => {
+  localStorage.removeItem('google');
+  return false;
+};
+
 export const validateToken = async (): Promise<Boolean> => {
   const token = localStorage.getItem('google');
   if (!token) return false;
 
   const googleURL = `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${token}`;
   const res = await fetch(googleURL);
+  if (res.status === 400) {
+    clearToken();
+  }
+
   const { exp } = await res.json();
 
   if (Date.now() > exp * 1000) {
-    localStorage.removeItem('google');
-    return false;
+    clearToken();
   }
   return true;
 };
